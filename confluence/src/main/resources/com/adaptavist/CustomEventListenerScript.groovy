81bf387084501673a251ea782baffac9f3ea0fc3
Jonny Carter <jcarter@adaptavist.com>
2023-06-29 15:21:49 -0500

    Fix confluence example to actually do anything

--- confluence/src/main/resources/com/adaptavist/CustomEventListenerScript.groovy
+++ confluence/src/main/resources/com/adaptavist/CustomEventListenerScript.groovy
@@ -1,7 +1,12 @@
 package com.adaptavist
 
 import com.atlassian.confluence.event.events.space.SpaceCreateEvent
+import com.atlassian.confluence.spaces.SpaceManager
+import com.atlassian.sal.api.component.ComponentLocator
+
+def spaceManager = ComponentLocator.getComponent(SpaceManager)
 
 def event = event as SpaceCreateEvent
 def space = event.space
 space.description.setBodyAsString("foo bar description")
+spaceManager.saveSpace(space)
